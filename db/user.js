30f814e2e8533fbf3129ac8fae6478fb5cf8eee2
Rotimi-Best <rotimiibitoyebest@yahoo.co.uk>
2019-01-02 22:03:09 +0200

    Added user db and cheatsheet files

---
+++ db/user.js
@@ -0,0 +1,45 @@
+const User = require("../models/user");
+
+const addUser = data => {
+  return new Promise((resolve, reject) => {
+    if (Array.isArray(data) && data.length) {
+      User.insertMany(data, (err, res) => {
+        if (err) reject(`Error while creating array: ${err}`);
+
+        resolve(res);
+      });
+    } else {
+      User.create(data, (err, res) => {
+        if (err) reject(`Error while creating one user: ${err}`);
+
+        resolve(res);
+      });
+    }
+  });
+};
+
+const findUser = params => {
+  return new Promise((resolve, reject) => {
+    User.find(params, (err, users) => {
+      if (err) reject(`Error while finding user ${err}`);
+
+      resolve(users);
+    });
+  });
+};
+
+const updateUser = (findField, setField) => {
+  return new Promise((resolve, reject) => {
+    User.update(findField, { $set: setField }, (err, res) => {
+      if (err) reject(`Error updating a user field ${err}`);
+
+      resolve(res);
+    });
+  });
+};
+
+module.exports = {
+  addUser,
+  findUser,
+  updateUser
+};

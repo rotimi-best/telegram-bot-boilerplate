23818c0c2b0a87e07bee3f240d32356bc404e1b5
Rotimi Best <rotimiibitoyebest@yahoo.co.uk>
2019-01-03 12:04:46 +0200

    Commented bot server

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const bot = require("./helpers/botConnection").get();
 const Start = require("./controllers/Start");
 const CallbackQueryController = require("./callbackQueries");
 
-bot.router.callbackQuery(new CallbackQueryController());
+// bot.router.callbackQuery(new CallbackQueryController());
 
-bot.router
-  .when(new TextCommand("/start", "startCommand"), new Start())
-  .otherwise(new OtherwiseController());
+// bot.router
+//   .when(new TextCommand("/start", "startCommand"), new Start())
+//   .otherwise(new OtherwiseController());
+
+console.log('Server started');
